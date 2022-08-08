126f6ffeae0e18b0048624ae80155f288178247c
Wozhungjie <63949859+18476305640@users.noreply.github.com>
2022-08-08 10:16:46 +0800

    'renren-fast加入到nacos服务发现'

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -12,7 +12,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient //开启服务发现 - nacos
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RenrenApplication {
 
